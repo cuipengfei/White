0c053d2e617b520ea513af6e996bfc6b83eb767d
Cui Pengfei <pfcui@thoughtworks.com>
2014-01-19 22:44:38 +0800

    use castle for .net 3.5, add tpl for .net 3.5 as dependency, add StringExtensions to fill the gap, change the Join call to match .net 3.5 signature of the api

---
+++ src/TestStack.White/Mappings/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace TestStack.White.Mappings
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrWhiteSpace(this string value)
+        {
+            return value == null || value.All(char.IsWhiteSpace);
+        }
+    }
+}
